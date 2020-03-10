443bd7654a3c746fd886e7ffe680fa9fe250dbdc
Petr-coder <59264340+Petr-coder@users.noreply.github.com>
2020-03-10 18:31:55 +0300

    Some changes

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,15 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainPage {
 
-    @FindBy(xpath = "")
+    @FindBy(xpath = "//DIV[@class='kit-col kit-col_xs_10 lg-menu__row']")
     WebElement mainMenu;
 
-    @FindBy(xpath = "")
+    @FindBy(xpath = "(//DIV[@class='kit-row'])[8]")
     WebElement subMenu;
 
+    public MainPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+    }
 
+    public void selectMainMenu(String menuItem){
+    mainMenu.findElement(By.xpath("//SPAN[@class='lg-menu__text'][text()='"+menuItem+"']"));
+}
+
+    public void selectSubMenu(String menuItem){
+    subMenu.findElement(By.xpath("(//A[@href='/ru/person/bank_inshure/insuranceprogram/life/travel'][text()='Страхование путешественников'][text()='\"+menuItem+\"'])[1]"));
+    }
 }
